2b5986a915fe32b53330b5fda85975219576fb84
Supakrit <supakrit@enres.co>
2023-02-13 20:56:50 +0700

    qr code component frontend mentor

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      mobile: "375px",
+      desktop: "1440px",
+    },
+    extend: {
+      colors: {
+        darkBlue: "hsl(218, 44%, 22%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        lightGray: "hsl(212, 45%, 89%)",
+      },
+    },
+  },
+  plugins: [],
+};
+
+// - White: hsl(0, 0%, 100%)
+// - Light gray: hsl(212, 45%, 89%)
+// - Grayish blue: hsl(220, 15%, 55%)
+// - Dark blue: hsl(218, 44%, 22%)
