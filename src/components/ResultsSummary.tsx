e663dcced990385c346f4d9d0819d234ac51171a
Supakrit <supakrit@enres.co>
2023-03-06 15:29:54 +0700

    Add ResultsSummary components

--- src/components/ResultsSummary.tsx
+++ src/components/ResultsSummary.tsx
@@ -3,10 +3,21 @@ import React from "react";
 const ResultsSummary = () => {
   return (
     <main className="flex justify-center  items-center h-screen font-hankenGrotesk">
-      <div className="flex flex-col rounded-3xl w-full shadow-xl overflow-hidden sm:flex-row sm:w-[620px]">
-        <div className="flex flex-col gap-5 items-center w-full bg-gradient-to-b from-lightSlateBlue to-lightRoyalBlue rounded-3xl pt-14 pb-10  pl-10 pr-10 sm:w-1/2 sm:pt-6">
+      <div
+        className="flex flex-col w-full h-full shadow-xl overflow-hidden sm:flex-row 
+        sm:w-[620px] sm:h-auto sm:rounded-3xl"
+      >
+        <div
+          className="flex flex-col gap-5 items-center w-full bg-gradient-to-b from-lightSlateBlue to-lightRoyalBlue 
+          rounded-t-none rounded-3xl pt-6 pb-10  pl-10 pr-10 
+          sm:w-1/2 sm:rounded-t-3xl"
+        >
           <span className="text-lg text-lightLavender">Your Result</span>
-          <div className="flex flex-col justify-center items-center text-white  w-32 h-32 bg-gradient-to-b from-violetBlue to-persianBlue rounded-full p-10 sm:w-40 sm:h-40">
+          <div
+            className="flex flex-col justify-center items-center text-white  w-32 h-32 bg-gradient-to-b from-violetBlue to-persianBlue 
+            rounded-full p-10 
+            sm:w-40 sm:h-40"
+          >
             <span className="text-5xl font-bold sm:text-6xl">76</span>
             <span className="text-grayishBlue">of 100</span>
           </div>
